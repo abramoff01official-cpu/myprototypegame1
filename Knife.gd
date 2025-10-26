860d86d580ac442b9eab873d1dbc305d45c6ea31
kirill <abramoff01official@gmail.com>
2025-10-26 16:38:51 +0300

    Knife.gd

--- Knife.gd
+++ Knife.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var item_name: String = "Knife"
 @export var item_icon: Texture
 @export var scene_path: String = "res://Scene/Knife.tscn"
+@export var damage: int = 5
+@export var attack_range: float = 28.0  # 🔪 короткий радиус атаки
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var highlight: Sprite2D = $Highlight
@@ -12,16 +14,15 @@ func _ready():
 	add_to_group("Item")
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	# скрываем подсветку
 	if highlight:
 		highlight.visible = false
 
 func _on_mouse_entered():
 	hovered = true
 	if highlight:
-		highlight.visible = true  # показываем рамку
+		highlight.visible = true
 
 func _on_mouse_exited():
 	hovered = false
 	if highlight:
-		highlight.visible = false  # скрываем рамку
+		highlight.visible = false
